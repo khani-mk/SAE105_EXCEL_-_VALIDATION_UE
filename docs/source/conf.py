09866bf1c70d8f33246b350b6859291f9349ba17
Karim HANI <root@I4-virtu>
2025-11-14 10:15:10 +0100

    Creation du répertoire docs (sphinx)

---
+++ docs/source/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'SAE105_EXCEL_-_VALIDATION_UE'
+copyright = '2025, Karim HANI Rémi Camelio'
+author = 'Karim HANI Rémi Camelio'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
