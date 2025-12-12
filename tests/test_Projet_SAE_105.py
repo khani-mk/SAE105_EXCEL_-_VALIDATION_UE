bb262d194946e2921e59d504cfb82ad93b43f3d2
RemiCamelio <remi.camelio86@gmail.com>
2025-12-12 10:55:03 +0000

    j'ai rajouté le import os

--- tests/test_Projet_SAE_105.py
+++ tests/test_Projet_SAE_105.py
@@ -1,19 +1,32 @@
 import openpyxl
+import os 
+
+
+list_des_fichiers=os.listdir('/workspaces/SAE105_EXCEL_-_VALIDATION_UE/tests/test/notes_S1')
+print(list_des_fichiers)
 
 workbook = openpyxl.load_workbook('/workspaces/SAE105_EXCEL_-_VALIDATION_UE/tests/test/notes_S1/Anglais_technique_1.xlsx', data_only = True)
 titres_onglets = workbook.sheetnames
 onglet1 = workbook[titres_onglets[0]]
+
+
 #Liste des lignes
 lignes = []
 for row in onglet1.values:
     #ajoute de la ligne row à la liste lignes
     lignes.append(list(row))
+
+
 #Liste des colonnes
 colonnes = []
 for column in onglet1.columns:
     colonnes.append([cell.value for cell in column])
+
+
 #ferme le fichier excel
 workbook.close()
+
+
 #Géneration de la page html
 def genere_page_web( nom_fichier, titre, corps):    
 
